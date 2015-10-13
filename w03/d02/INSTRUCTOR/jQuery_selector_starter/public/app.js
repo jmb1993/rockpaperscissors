c3cde824b1e73ed0912650e9174442b13ccb42f4
William Shaw <williamshaw@WilliamShaw.local>
2015-10-13 15:14:40 -0400

    added trillo starter

--- w03/d02/INSTRUCTOR/jQuery_selector_starter/public/app.js
+++ w03/d02/INSTRUCTOR/jQuery_selector_starter/public/app.js
@@ -1,3 +1,36 @@
 $(function(){
 	console.log('Loaded, bro');
+	var h1Tag = $('h1');
+	var divs = $('div');
+	var liTags = $('li');
+	var swag = $('li.swag');
+	var awesome = $('#awesome');
+	var aTag = $('li a');
+	var changeColor = $('.circle').css('background', 'purple');
 });
+
+
+// // VANILLA
+// var aside = document.getElementById("awesome");
+// //jQuery
+// var aside = $("#awesome");
+//
+// // VANILLA
+// var pTags = document.getElementsByTagName("p");
+// //jQuery
+// var pTagsJquery = $('p');
+//
+// //VANILLA
+// var classGrab = document.getElementsByClassName("circle");
+// //jQuery
+// var classGrab = $('.circle');
+//
+// //VANILLA
+// var createdElement = document.createElement("div");
+// //jQuery
+// var createdElementJquery = $('<div>');
+//
+// //VANILLA
+// createdElement.innerHTML = "Hello WDI";
+// //jQuery
+// createdElementJquery.text("Hello WDI");
