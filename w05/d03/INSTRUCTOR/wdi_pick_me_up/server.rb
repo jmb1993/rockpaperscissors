954c75acdfec33e68c494c00f1e101fc58b020c1
Dennis Liaw <dennis.liaw@gmail.com>
2015-10-29 09:33:06 -0400

    adds wdi pick me up solution

---
+++ w05/d03/INSTRUCTOR/wdi_pick_me_up/server.rb
@@ -0,0 +1,27 @@
+module Wdi
+
+  class Server < Sinatra::Base
+    $compliments = [
+  "People behind you in class think you are the perfect height.",
+  "Your instructors think you're young.",
+  "Your posture during breaks effectively masks your exhaustion.",
+  "Your commit messages are Kafka-esque",
+  "Your fingers are magic on the keys",
+  ]
+    get('/') do
+      @color = ("%06x" % (rand * 0xffffff)).to_s
+      @compliment = $compliments.sample
+      render( :erb, :index)
+    end
+
+    get('/:name') do
+      @color = ("%06x" % (rand * 0xffffff)).to_s
+      @name = params[:name].capitalize
+      @compliment = $compliments.sample
+      render( :erb, :index)
+    end
+
+
+  end#Server
+
+end#Portfolio
