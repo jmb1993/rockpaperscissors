6747191172d20b71ef598bda164bf14e999c0252
William Shaw <williamshaw@WilliamShaw.local>
2015-11-02 12:28:16 -0500

    tunr mid day

--- w06/d01/ASSIGNMENT/tunr_app/app/models/user.rb
+++ w06/d01/ASSIGNMENT/tunr_app/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
-  validates :username, presence: true
-  validates :email, presence: true
+  before_save {self.email = email.downcase}
+  validates :username, presence: true, length: {maximum: 50}
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 255}, format: {with: EMAIL_REGEX}, uniqueness: {case_sensitive: false}
 end
