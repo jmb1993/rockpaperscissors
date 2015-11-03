8d4630f416c93a20a1d7c7cb840c346f69721560
William Shaw <williamshaw@WilliamShaw.local>
2015-11-03 14:49:09 -0500

    added tuner controller

---
+++ w06/d01/ASSIGNMENT/tunr_app/app/controllers/songs_controller.rb
@@ -0,0 +1,53 @@
+class SongsController < ApplicationController
+  # index
+    def index
+       @songs = Song.all
+    end
+
+    # create
+    def create
+       @song = Song.new(song_params)
+       if @song.save
+          redirect_to song_path(@song)
+       else
+          redirect_to new_song_path
+       end
+    end
+
+    # new
+    def new
+       @artists = Artist.all
+       @song = Song.new
+    end
+
+    # edit
+    def edit
+       @song = Song.find(params[:id])
+       @artists = Artist.all
+    end
+
+    # show
+    def show
+       @song = Song.find(params[:id])
+    end
+
+    # update
+    def update
+       @song = Song.find(params[:id])
+       @song.update(song_params)
+       redirect_to @song
+    end
+
+    # destroy
+    def destroy
+       #destroy actions
+       @song = Song.find(params[:id])
+       @song.destroy
+       redirect_to songs_path
+    end
+
+    private
+    def song_params
+       params.require(:song).permit(:title, :album, :preview_url, :artwork, :price, :artist_id)
+    end
+end
--- w06/d01/ASSIGNMENT/tunr_app/config/routes.rb
+++ w06/d01/ASSIGNMENT/tunr_app/config/routes.rb
@@ -1,8 +1,32 @@
 Rails.application.routes.draw do
 
+  get 'artists/new'
+
+  get 'artists/edit'
+
+  get 'artists/index'
+
+  get 'artists/show'
+
+  get 'songs/new'
+
+  get 'songs/edit'
+
+  get 'songs/index'
+
+  get 'songs/show'
+
   get 'login' => "sessions#new"
   post 'login' => "sessions#create"
   delete 'logout' => "sessions#destroy"
   root to: "sessions#new"
-  resources :users
+  resources :users, :songs, :artists
+
+  resources :playlists do
+    member do
+      put 'add_song'
+      put 'remove_song'
+    end
+  end
+
 end
