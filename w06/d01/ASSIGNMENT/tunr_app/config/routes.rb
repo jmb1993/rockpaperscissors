b8729ad988162dd8ae3262f617a72dc065d6c8be
James M. Berick <james@James-Bericks-MacBook-Pro-3.local>
2015-11-03 10:45:59 -0500

    (routes edited)

--- w06/d01/ASSIGNMENT/tunr_app/config/routes.rb
+++ w06/d01/ASSIGNMENT/tunr_app/config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  
-=======
+
 
   get 'login' => "sessions#new"
   post 'login' => "sessions#create"
   delete 'logout' => "sessions#destroy"
   root to: "sessions#new"
   resources :users
->>>>>>> ff38cf10060af6a8de225975401fe987cd1f0e9d
 end
