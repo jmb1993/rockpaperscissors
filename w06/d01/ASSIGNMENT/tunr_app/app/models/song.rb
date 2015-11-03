b8df6e0fc0962b9f0e37026ab8b43a3dd577414c
William Shaw <williamshaw@WilliamShaw.local>
2015-11-03 12:26:06 -0500

    added tuner lunch time

--- w06/d01/ASSIGNMENT/tunr_app/app/models/song.rb
+++ w06/d01/ASSIGNMENT/tunr_app/app/models/song.rb
@@ -1,2 +1,9 @@
 class Song < ActiveRecord::Base
+
+ belongs_to :artist
+ has_and_belongs_to_many :playlists
+
+ before_save {self.title = title.downcase, self.album = album.downcase, self.artwork = artwork.downcase}
+ validates :title, :album, :preview_url, :artwork, presence: true
+ validates :price, presence: true, numericality: true, inclusion: { in: 1..99}
 end
--- w06/d01/ASSIGNMENT/tunr_app/db/schema.rb
+++ w06/d01/ASSIGNMENT/tunr_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151103155324) do
+ActiveRecord::Schema.define(version: 20151103172113) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20151103155324) do
     t.string   "nationality"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "genre"
   end
 
   create_table "playlists", force: :cascade do |t|
