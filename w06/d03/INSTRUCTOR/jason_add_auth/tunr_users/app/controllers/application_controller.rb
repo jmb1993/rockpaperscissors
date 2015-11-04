d5f1316add27e8764aaab0b0767e9a48a2536b32
jasonseminara <jason.seminara@generalassemb.lyexit>
2015-11-04 14:21:54 -0500

    In-class work to install authorization and authentication using sessions.

--- w06/d03/INSTRUCTOR/jason_add_auth/tunr_users/app/controllers/application_controller.rb
+++ w06/d03/INSTRUCTOR/jason_add_auth/tunr_users/app/controllers/application_controller.rb
@@ -3,4 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  before_action :authenticate
+
+   def authenticate
+     unless logged_in?
+       flash[:error] = "You must be logged in to access this section of the site"
+       redirect_to login_path
+     end
+   end
+
 end
