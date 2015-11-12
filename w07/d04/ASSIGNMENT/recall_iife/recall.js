76c46f52b97232b22d9e21cd7032978e6713f933
James M. Berick <james@James-Bericks-MacBook-Pro-3.local>
2015-11-12 10:46:19 -0500

    (recall)

---
+++ w07/d04/ASSIGNMENT/recall_iife/recall.js
@@ -0,0 +1,16 @@
+function recursion(param1, param2, callback){
+console.log(param1 + ' ' + param2 );
+callback();
+};
+recursion("hello", "word",function(param3){
+console.log(recursion()+ param3)
+});
+
+
+
+var recall = function(word){
+  var sentence = ' ';
+  var chatter = function(newWord, callback){
+    if (typoof newWord)
+  }
+};
