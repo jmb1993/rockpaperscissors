d833bd7d01c5427199c4513bf7f3ec6d047c4829
jasonseminara <jason.seminara@generalassemb.lyexit>
2015-11-15 22:41:20 -0500

    added routes defined in hw

--- w07/d05/ASSIGNMENT/pokemon_node/server.js
+++ w07/d05/ASSIGNMENT/pokemon_node/server.js
@@ -3,10 +3,38 @@ var express = require('express'),
     app = express(),
     pokemons = require('./poke_array')
 
+var trySendData=(item,res)=> item ? res.send(item) : res.sendStatus(404).end()
+
 app.use(logger('dev'));
 app.use(express.static('public'));   
 
 app.listen(3000,(req,res)=>'hello world')
-app.get('/',(req,res)=>res.render('index.html'))
 
-app.get('/pokemons', (req,res)=>res.send(pokemons));
+
+app.get('/',(req,res)=>res.render('index.html'))
+app.get('/pokemons/searchByName', (req,res)=>{
+  var curr; 
+  if (req.query.name) curr = pokemons.find( el=> el.name.toLowerCase() === req.query.name.toLowerCase() );
+  trySendData(curr,res);
+})
+
+app.get('/pokemons/searchByType', (req,res)=>{
+  var curr;
+  if (req.query.type) curr = pokemons.filter( el=>el.type.some( ty=> ty.toLowerCase()===req.query.type.toLowerCase())) 
+  curr.forEach(el=>console.log(el.type))
+  trySendData(curr,res);
+})
+
+app.get('/pokemons/random', (req,res)=>{
+  var rando = Math.floor(Math.random() * (pokemons.length + 1));
+  trySendData(pokemons[rando],res);
+})
+
+app.get('/pokemons/:id',(req,res)=>{
+  var curr = pokemons.find( el=> +el.id === +req.params.id )
+  trySendData(curr,res);
+});
+
+app.get('/pokemons', (req,res)=>{
+  res.send(pokemons)
+});
