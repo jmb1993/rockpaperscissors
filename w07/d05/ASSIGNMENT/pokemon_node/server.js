d557ca6e968e78046eda6add0188986e842f498e
jasonseminara <jason.seminara@generalassemb.lyexit>
2015-11-17 09:53:17 -0500

    split off controller

--- w07/d05/ASSIGNMENT/pokemon_node/server.js
+++ w07/d05/ASSIGNMENT/pokemon_node/server.js
@@ -2,12 +2,11 @@
 var express     = require('express'),
     logger      = require('morgan'),
     app         = express(),
+    router      = express.Router(),
     favicon     = require('serve-favicon'),
-    pokemons    = require('./poke_array'),
     bodyParser  = require('body-parser'),
-    session     = require('express-session');
-
-var trySendData=(item,res)=> item ? res.send(item) : res.sendStatus(404).end()
+    session     = require('express-session'),
+    pokemons    = require('./controllers/pokemons.js');
 
 app.use(logger('dev'));
 app.use(express.static('public'));   
@@ -22,44 +21,10 @@ app.use(session({
   resave: false
 }));
 
-app.listen(3000,(req,res)=>'hello world')
+
+
 
 app.get('/',(req,res)=>res.render('index.html'))
 
-
-app.get('/pokemons/searchByName', (req,res)=>{
-  var curr; 
-  if(req.query.name) {
-    curr = pokemons.find( function(el){
-      return el.name.toLowerCase() === req.query.name.toLowerCase() 
-    });
-  }
-  trySendData(curr,res);
-})
-
-app.get('/pokemons/searchByType', (req,res)=>{
-  var curr;
-  if(req.query.type){
-    curr = pokemons.filter( function(el){
-      return el.type.some( function(ty){ 
-        return ty.toLowerCase() === req.query.type.toLowerCase() 
-      })
-    }) 
-  } 
-  curr.forEach( el=>console.log(el.type) )
-  trySendData(curr,res);
-})
-
-app.get('/pokemons/random', (req,res)=>{
-  var rando = Math.floor(Math.random() * (pokemons.length + 1));
-  trySendData(pokemons[rando],res);
-})
-
-app.get('/pokemons/:id',(req,res)=>{
-  var curr = pokemons.find( el=> +el.id === +req.params.id )
-  trySendData(curr,res);
-});
-
-app.get('/pokemons', (req,res)=>{
-  res.send(pokemons)
-});
+app.listen(3000,(req,res)=>'hello world')
+app.use('/', pokemons);
